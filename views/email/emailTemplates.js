9f78e1d32e507d82272213031b52a3fff51ec360
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-03-27 00:28:35 +0100

    started ammending emails styles to make it simpler

--- views/email/emailTemplates.js
+++ views/email/emailTemplates.js
@@ -20,142 +20,121 @@ module.exports = class Template {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     
         <style>
-          *,
-          *::after,
-          *::before {
-            padding: 0;
-            margin: 0;
-            box-sizing: inherit;
-          }
-    
-          html {
-            font-size: 62.5%;
-            box-sizing: border-box;
-          }
-    
-          body {
-            font-family: 'bookmania', serif;
-          }
-    
-          .container {
-            width: 100%;
-          }
-    
-          .header,
-          .footer {
-            display: table;
-            margin: 0 auto;
-            width: 60%;
-            height: 8rem;
-            background-color: #58b15a;
-          }
-    
-          h1 {
-            font-size: 3rem;
-            padding-bottom: 1rem;
-          }
-    
-          h2 {
-            font-size: 2rem;
-          }
-    
-          .title {
-            text-align: center;
-            padding-top: 2rem;
-          }
-    
-          .main {
-            margin: 0 auto;
-            padding: 2.4rem;
-            width: 60%;
-            height: 100%;
-            background-color: #d2dad2;
-          }
-    
-          .main-body {
-            text-align: center;
-            height: 100%;
-          }
-    
-          .main-body-para {
-            font-size: 1.8rem;
-            padding-top: 2rem;
-            padding-bottom: 2rem;
-          }
-    
-          .foooter-title {
-            text-align: left;
-            padding: 2rem;
-          }
-    
-          .img-header {
-            display: block;
-            margin: 0 auto;
-          }
-    
-          .footer {
-            position: relative;
-            height: 12rem;
-          }
-    
-          .img-footer {
-            display: block;
-            margin-left: 4rem;
-          }
-    
-          .footer span {
-            position: absolute;
-            margin-left: 7rem;
-            bottom: 2rem;
-          }
-    
-          .button-link,
-          .button-link:link,
-          .button-link:active {
-            color: #58b15a;
-          }
-    
-          .footer-link,
-          .footer-link:link,
-          .footer-link:active {
-            position: absolute;
-            margin-left: 7rem;
-            bottom: 2rem;
-    
-            font-size: 1rem;
-            color: rgb(0, 0, 0);
-            text-decoration: underline;
-          }
-    
-          .footer-link,
-          .footer-link:hover,
-          .footer-link:visited {
-            position: absolute;
-            margin-left: 7rem;
-            bottom: 2rem;
-    
-            font-size: 1rem;
-            color: #d2dad2;
-          }
-    
-          button {
-            height: 5rem;
-            width: 18rem;
-            padding: 1rem 3rem 1rem 3rem;
-            border: none;
-            background-color: black;
-            font-family: inherit;
-    
-            cursor: pointer;
-          }
-    
-          a,
-          a:link,
-          a:active {
-            text-decoration: none;
-            font-size: 1.6rem;
-          }
-        </style>
+        html {
+          font-size: 62.5%;
+        }
+  
+        .container {
+          width: 100%;
+        }
+  
+        .header,
+        .footer {
+          display: table;
+          margin: 0 auto;
+          width: 40rem;
+          height: 8rem;
+        }
+  
+        h1 {
+          font-size: 2.4rem;
+        }
+  
+        h2 {
+          font-size: 2rem;
+        }
+  
+        .title {
+          text-align: center;
+          padding-top: 2rem;
+        }
+  
+        .main {
+          margin: 0 auto;
+          width: 40rem;
+        }
+  
+        .main-body {
+          text-align: center;
+        }
+  
+        .main-body-para {
+          margin: 0 auto;
+          width: 80%;
+          font-size: 1.8rem;
+          padding-top: 2rem;
+          padding-bottom: 2rem;
+        }
+  
+        .foooter-title {
+          text-align: left;
+          padding: 2rem;
+        }
+  
+        .img-header {
+          display: block;
+          margin: 0 auto;
+        }
+  
+        .footer {
+          position: relative;
+          height: 12rem;
+        }
+  
+        .img-footer {
+          display: block;
+          margin-left: 4rem;
+        }
+  
+        .footer span {
+          position: absolute;
+          margin-left: 7rem;
+        }
+  
+        .button-link {
+          color: #58b15a;
+        }
+  
+        .footer-link {
+          position: absolute;
+          margin-left: 7rem;
+          bottom: 2rem;
+  
+          font-size: 1rem;
+          color: rgb(0, 0, 0);
+          text-decoration: underline;
+        }
+  
+        .footer-link {
+          position: absolute;
+          margin-left: 7rem;
+  
+          font-size: 1rem;
+          color: #d2dad2;
+        }
+  
+        .leaf-icon {
+          width: 20px;
+          color: #58b15a;
+        }
+  
+        button {
+          height: 5rem;
+          width: 18rem;
+          padding: 1rem 3rem 1rem 3rem;
+          border: none;
+          background-color: black;
+  
+          font-family: inherit;
+  
+          cursor: pointer;
+        }
+  
+        a {
+          text-decoration: none;
+          font-size: 1.6rem;
+        }
+      </style>
     
         <title>${this.options.subject}</title>
       </head>
@@ -216,151 +195,22 @@ module.exports = class Template {
     <!DOCTYPE html>
     <html lang="en">
       <head>
-        <style>
-          *,
-          *::after,
-          *::before {
-            padding: 0;
-            margin: 0;
-            box-sizing: inherit;
-          }
-    
-          html {
-            font-size: 62.5%;
-            box-sizing: border-box;
-          }
-    
-          body {
-            font-family: 'bookmania', serif;
-          }
-    
-          .container {
-            width: 100%;
-          }
-    
-          .header,
-          .footer {
-            display: table;
-            margin: 0 auto;
-            width: 60%;
-            height: 8rem;
-            background-color: #58b15a;
-          }
-    
-          h1 {
-            font-size: 2.4rem;
-            padding-bottom: 1rem;
-          }
-    
-          h2 {
-            font-size: 2rem;
-          }
-    
-          .title {
-            text-align: center;
-            padding-top: 2rem;
-          }
-    
-          .main {
-            margin: 0 auto;
-            padding: 2.4rem;
-            width: 60%;
-            height: 100%;
-            background-color: #d2dad2;
-          }
-    
-          .main-body {
-            text-align: center;
-            height: 100%;
-          }
-    
-          .main-body-para {
-            margin: 0 auto;
-            width: 80%;
-            font-size: 1.8rem;
-            padding-top: 2rem;
-            padding-bottom: 2rem;
-          }
-    
-          .foooter-title {
-            text-align: left;
-            padding: 2rem;
-          }
-    
-          .img-header {
-            display: block;
-            margin: 0 auto;
-          }
-    
-          .footer {
-            position: relative;
-            height: 12rem;
-          }
-    
-          .img-footer {
-            display: block;
-            margin-left: 4rem;
-          }
-    
-          .footer span {
-            position: absolute;
-            margin-left: 7rem;
-            bottom: 2rem;
-          }
-    
-          .button-link,
-          .button-link:link,
-          .button-link:active {
-            color: #58b15a;
-          }
-    
-          .footer-link,
-          .footer-link:link,
-          .footer-link:active {
-            position: absolute;
-            margin-left: 7rem;
-            bottom: 2rem;
-    
-            font-size: 1rem;
-            color: rgb(0, 0, 0);
-            text-decoration: underline;
-          }
-    
-          .footer-link,
-          .footer-link:hover,
-          .footer-link:visited {
-            position: absolute;
-            margin-left: 7rem;
-            bottom: 2rem;
-    
-            font-size: 1rem;
-            color: #d2dad2;
-          }
-    
-          .leaf-icon {
-            width: 20px;
-            color: #58b15a;
-          }
-    
-          button {
-            height: 5rem;
-            width: 18rem;
-            padding: 1rem 3rem 1rem 3rem;
-            border: none;
-            background-color: black;
-    
-            font-family: inherit;
-    
-            cursor: pointer;
-          }
-    
-          a,
-          a:link,
-          a:active {
-            text-decoration: none;
-            font-size: 1.6rem;
-          }
-        </style>
+      <style>
+      html {
+        font-size: 62.5%;
+        color: black;
+      }
+
+      a {
+        color: white;
+      }
+
+      button {
+        color: white;
+        background: grey;
+        border: none;
+      }
+    </style>
     
         <meta charset="UTF-8" />
         <meta http-equiv="Content-Type" , content="text/html; charset=UTF-8" />
@@ -370,9 +220,6 @@ module.exports = class Template {
       </head>
       <body>
         <div class="container">
-          <header class="header">
-            <img class="img-header" src="${this.options.url}/public/emailImgs/vesearch-logo.png" alt="VESearch logo" height="120px" />
-          </header>
     
           <main class="main">
             <div class="main-body">
@@ -383,21 +230,7 @@ module.exports = class Template {
                 <br />
                 Below you'll find a bit of information about this web app:
               </p>
-              <img
-                class="leaf-icon"
-                src="${this.options.url}/public/emailImgs/leaf-icon.svg"
-                alt="leaf icon svg"
-              />
-              <img
-                class="leaf-icon"
-                src="${this.options.url}/public/emailImgs/leaf-icon.svg"
-                alt="leaf icon svg"
-              />
-              <img
-                class="leaf-icon"
-                src="${this.options.url}/public/emailImgs/leaf-icon.svg"
-                alt="leaf icon svg"
-              />
+             
               <p class="main-body-para">
                 This vegan recipe search engine web application was built by James
                 Kevin Woodruff as part of a learning process. He built the backend
@@ -411,13 +244,7 @@ module.exports = class Template {
               </button>
             </div>
           </main>
-    
-          <footer class="footer">
-            <img class="img-footer" src="${this.options.url}/public/emailImgs/vesearch-logo.png" alt="VESearch logo" height="100px" />
-            <a class="footer-link" href="https://www.jameskevinwoodruff.com"
-              >By Atomic Mammoth Media</a
-            >
-          </footer>
+  
         </div>
       </body>
     </html>
@@ -428,151 +255,122 @@ module.exports = class Template {
     return `<!DOCTYPE html>
     <html lang="en">
       <head>
-        <style>
-          *,
-          *::after,
-          *::before {
-            padding: 0;
-            margin: 0;
-            box-sizing: inherit;
-          }
-    
-          html {
-            font-size: 62.5%;
-            box-sizing: border-box;
-          }
-    
-          body {
-            font-family: 'bookmania', serif;
-          }
-    
-          .container {
-            width: 100%;
-          }
-    
-          .header,
-          .footer {
-            display: table;
-            margin: 0 auto;
-            width: 60%;
-            height: 8rem;
-            background-color: #58b15a;
-          }
-    
-          h1 {
-            font-size: 2.4rem;
-            padding-bottom: 1rem;
-          }
-    
-          h2 {
-            font-size: 2rem;
-          }
-    
-          .title {
-            text-align: center;
-            padding-top: 2rem;
-          }
-    
-          .main {
-            margin: 0 auto;
-            padding: 2.4rem;
-            width: 60%;
-            height: 100%;
-            background-color: #d2dad2;
-          }
-    
-          .main-body {
-            text-align: center;
-            height: 100%;
-          }
-    
-          .main-body-para {
-            margin: 0 auto;
-            width: 80%;
-            font-size: 1.8rem;
-            padding-top: 2rem;
-            padding-bottom: 2rem;
-          }
-    
-          .foooter-title {
-            text-align: left;
-            padding: 2rem;
-          }
-    
-          .img-header {
-            display: block;
-            margin: 0 auto;
-          }
-    
-          .footer {
-            position: relative;
-            height: 12rem;
-          }
-    
-          .img-footer {
-            display: block;
-            margin-left: 4rem;
-          }
-    
-          .footer span {
-            position: absolute;
-            margin-left: 7rem;
-            bottom: 2rem;
-          }
-    
-          .button-link,
-          .button-link:link,
-          .button-link:active {
-            color: #58b15a;
-          }
-    
-          .footer-link,
-          .footer-link:link,
-          .footer-link:active {
-            position: absolute;
-            margin-left: 7rem;
-            bottom: 2rem;
-    
-            font-size: 1rem;
-            color: rgb(0, 0, 0);
-            text-decoration: underline;
-          }
-    
-          .footer-link,
-          .footer-link:hover,
-          .footer-link:visited {
-            position: absolute;
-            margin-left: 7rem;
-            bottom: 2rem;
-    
-            font-size: 1rem;
-            color: #d2dad2;
-          }
-    
-          .leaf-icon {
-            width: 20px;
-            color: #58b15a;
-          }
-    
-          button {
-            height: 5rem;
-            width: 18rem;
-            padding: 1rem 3rem 1rem 3rem;
-            border: none;
-            background-color: black;
-    
-            font-family: inherit;
-    
-            cursor: pointer;
-          }
-    
-          a,
-          a:link,
-          a:active {
-            text-decoration: none;
-            font-size: 1.6rem;
-          }
-        </style>
+      <style>
+      html {
+        font-size: 62.5%;
+      }
+
+      .container {
+        width: 100%;
+      }
+
+      .header,
+      .footer {
+        display: table;
+        margin: 0 auto;
+        width: 40rem;
+        height: 8rem;
+      }
+
+      h1 {
+        font-size: 2.4rem;
+      }
+
+      h2 {
+        font-size: 2rem;
+      }
+
+      .title {
+        text-align: center;
+        padding-top: 2rem;
+      }
+
+      .main {
+        margin: 0 auto;
+        width: 40rem;
+      }
+
+      .main-body {
+        text-align: center;
+      }
+
+      .main-body-para {
+        margin: 0 auto;
+        width: 80%;
+        font-size: 1.8rem;
+        padding-top: 2rem;
+        padding-bottom: 2rem;
+      }
+
+      .foooter-title {
+        text-align: left;
+        padding: 2rem;
+      }
+
+      .img-header {
+        display: block;
+        margin: 0 auto;
+      }
+
+      .footer {
+        position: relative;
+        height: 12rem;
+      }
+
+      .img-footer {
+        display: block;
+        margin-left: 4rem;
+      }
+
+      .footer span {
+        position: absolute;
+        margin-left: 7rem;
+      }
+
+      .button-link {
+        color: #58b15a;
+      }
+
+      .footer-link {
+        position: absolute;
+        margin-left: 7rem;
+        bottom: 2rem;
+
+        font-size: 1rem;
+        color: rgb(0, 0, 0);
+        text-decoration: underline;
+      }
+
+      .footer-link {
+        position: absolute;
+        margin-left: 7rem;
+
+        font-size: 1rem;
+        color: #d2dad2;
+      }
+
+      .leaf-icon {
+        width: 20px;
+        color: #58b15a;
+      }
+
+      button {
+        height: 5rem;
+        width: 18rem;
+        padding: 1rem 3rem 1rem 3rem;
+        border: none;
+        background-color: black;
+
+        font-family: inherit;
+
+        cursor: pointer;
+      }
+
+      a {
+        text-decoration: none;
+        font-size: 1.6rem;
+      }
+    </style>
     
         <meta charset="UTF-8" />
         <meta http-equiv="Content-Type" , content="text/html; charset=UTF-8" />
