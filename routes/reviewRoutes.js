25624417de9f44255c378d2361482e3051fd3347
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-03-26 16:47:40 +0100

    removed restrictions on review route

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,16 +4,13 @@ const {
   getAllReviews,
   ratingsAverage,
 } = require('../controllers/reviewController');
-const { protect, restrictTo } = require('../controllers/authController');
+const { protect } = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/ratingsAverage/:recipeId').get(ratingsAverage);
 
 // the rerouted route from the recipe route will come in here along with the params
-router
-  .route('/')
-  .post(protect, addReview)
-  .get(protect, restrictTo('admin'), getAllReviews);
+router.route('/').post(protect, addReview).get(protect, getAllReviews);
 
 module.exports = router;
