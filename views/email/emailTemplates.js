bb02a6dcdd2475f5abc9bf7e037e7348e21dd97b
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-03-29 00:04:47 +0100

    updated email templates to make them simpler

--- views/email/emailTemplates.js
+++ views/email/emailTemplates.js
@@ -12,181 +12,60 @@ module.exports = class Template {
 
   passwordReset() {
     return `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="Content-Type" , content="text/html; charset=UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    
-        <style>
+   
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <style>
         html {
           font-size: 62.5%;
-        }
-  
-        .container {
-          width: 100%;
-        }
-  
-        .header,
-        .footer {
-          display: table;
-          margin: 0 auto;
-          width: 40rem;
-          height: 8rem;
-        }
-  
-        h1 {
-          font-size: 2.4rem;
-        }
-  
-        h2 {
-          font-size: 2rem;
-        }
-  
-        .title {
-          text-align: center;
-          padding-top: 2rem;
-        }
-  
-        .main {
-          margin: 0 auto;
-          width: 40rem;
-        }
-  
-        .main-body {
-          text-align: center;
-        }
-  
-        .main-body-para {
-          margin: 0 auto;
-          width: 80%;
-          font-size: 1.8rem;
-          padding-top: 2rem;
-          padding-bottom: 2rem;
-        }
-  
-        .foooter-title {
-          text-align: left;
-          padding: 2rem;
-        }
-  
-        .img-header {
-          display: block;
-          margin: 0 auto;
-        }
-  
-        .footer {
-          position: relative;
-          height: 12rem;
-        }
-  
-        .img-footer {
-          display: block;
-          margin-left: 4rem;
-        }
-  
-        .footer span {
-          position: absolute;
-          margin-left: 7rem;
-        }
-  
-        .button-link {
-          color: #58b15a;
-        }
-  
-        .footer-link {
-          position: absolute;
-          margin-left: 7rem;
-          bottom: 2rem;
-  
-          font-size: 1rem;
-          color: rgb(0, 0, 0);
-          text-decoration: underline;
-        }
-  
-        .footer-link {
-          position: absolute;
-          margin-left: 7rem;
-  
-          font-size: 1rem;
-          color: #d2dad2;
-        }
-  
-        .leaf-icon {
-          width: 20px;
-          color: #58b15a;
-        }
-  
-        button {
-          height: 5rem;
-          width: 18rem;
-          padding: 1rem 3rem 1rem 3rem;
-          border: none;
-          background-color: black;
-  
-          font-family: inherit;
-  
-          cursor: pointer;
+          color: black;
+          width: 50%;
+          font-family: Georgia, 'Times New Roman', Times, serif;
         }
   
         a {
+          font-family: inherit;
+          color: black;
           text-decoration: none;
-          font-size: 1.6rem;
+        }
+  
+        p {
+          font-size: 1.4rem;
+        }
+  
+        button {
+          padding: 1rem;
+          background: #58b15a;
+          border: none;
         }
       </style>
-    
-        <title>${this.options.subject}</title>
-      </head>
-      <body>
-        <div class="container">
-          <header class="header">
-            <img
-              class="img-header"
-              src="${
-                this.options.url.split('/new')[0]
-              }/public/emailImgs/vesearch-logo.png"
-              height="120px"
-              alt="VESearch logo"
-            />
-          </header>
-    
-          <main class="main">
-            <div class="main-body">
-              <h1 class="title">Your Password Reset Link Has Arrived!</h1>
-              <p class="main-body-para">
-                Hey, ${
-                  this.options.username
-                }! Please find below your password reset link:
-              </p>
-              <button class="main-body-link">
-                <a class="button-link" href="${
-                  this.options.url
-                }">Reset Password</a>
-              </button>
-              <p class="main-body-para">
-                If you didn't request this link, please simply ignore it.
-              </p>
-            </div>
-          </main>
-    
-          <footer class="footer">
-            <img
-              class="img-footer"
-              src="${
-                this.options.url.split('/new')[0]
-              }/public/emailImgs/vesearch-logo.png"
-              height="100px"
-              alt="VESearch logo"
-            />
-            <a class="footer-link" href="https://www.jameskevinwoodruff.com"
-              >By Atomic Mammoth Media</a
-            >
-          </footer>
-        </div>
-      </body>
-    </html>
-    
+  
+      <meta charset="UTF-8" />
+      <meta http-equiv="Content-Type" , content="text/html; charset=UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  
+      <title>${this.options.subject}</title>
+    </head>
+    <body>
+      <div>
+      <main>
+      <div>
+        <h1>Your Password Reset Link Has Arrived!</h1>
+        <p>
+          Hey, ${this.options.username}! Please find below your password reset link:
+        </p>
+        <button>
+          <a class="button-link" href="${this.options.url}">Reset Password</a>
+        </button>
+        <p>
+          If you didn't request this link, please simply ignore it.
+        </p>
+      </div>
+    </main>
+      </div>
+    </body>
+  </html>
     `;
   }
 
@@ -195,22 +74,30 @@ module.exports = class Template {
     <!DOCTYPE html>
     <html lang="en">
       <head>
-      <style>
-      html {
-        font-size: 62.5%;
-        color: black;
-      }
-
-      a {
-        color: white;
-      }
-
-      button {
-        color: white;
-        background: grey;
-        border: none;
-      }
-    </style>
+        <style>
+          html {
+            font-size: 62.5%;
+            color: black;
+            width: 50%;
+            font-family: Georgia, 'Times New Roman', Times, serif;
+          }
+    
+          a {
+            font-family: inherit;
+            color: black;
+            text-decoration: none;
+          }
+    
+          p {
+            font-size: 1.4rem;
+          }
+    
+          button {
+            padding: 1rem;
+            background: #58b15a;
+            border: none;
+          }
+        </style>
     
         <meta charset="UTF-8" />
         <meta http-equiv="Content-Type" , content="text/html; charset=UTF-8" />
@@ -219,19 +106,18 @@ module.exports = class Template {
         <title>${this.options.subject}</title>
       </head>
       <body>
-        <div class="container">
-    
-          <main class="main">
-            <div class="main-body">
+        <div>
+          <div>
+            <div>
               <h1>Hey, ${this.options.username}! 👋</h1>
-              <p class="main-body-para">
+              <p>
                 Welcome to VESearch!
                 <br />
                 <br />
                 Below you'll find a bit of information about this web app:
               </p>
-             
-              <p class="main-body-para">
+    
+              <p>
                 This vegan recipe search engine web application was built by James
                 Kevin Woodruff as part of a learning process. He built the backend
                 API using Node.js and the frontend using React.js in an attempt to
@@ -239,12 +125,11 @@ module.exports = class Template {
                 the edges. Please report any issues you find with the app to
                 soygazer(at)gmail.com.
               </p>
-              <button class="main-body-link">
-                <a class="button-link" href="${this.options.url}">Try VESearch</a>
+              <button>
+                <a href="${this.options.url}">Try VESearch</a>
               </button>
             </div>
-          </main>
-  
+          </div>
         </div>
       </body>
     </html>
@@ -252,187 +137,71 @@ module.exports = class Template {
   }
 
   deletionMessage() {
-    return `<!DOCTYPE html>
-    <html lang="en">
-      <head>
-      <style>
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <style>
       html {
         font-size: 62.5%;
-      }
-
-      .container {
-        width: 100%;
-      }
-
-      .header,
-      .footer {
-        display: table;
-        margin: 0 auto;
-        width: 40rem;
-        height: 8rem;
-      }
-
-      h1 {
-        font-size: 2.4rem;
-      }
-
-      h2 {
-        font-size: 2rem;
-      }
-
-      .title {
-        text-align: center;
-        padding-top: 2rem;
-      }
-
-      .main {
-        margin: 0 auto;
-        width: 40rem;
-      }
-
-      .main-body {
-        text-align: center;
-      }
-
-      .main-body-para {
-        margin: 0 auto;
-        width: 80%;
-        font-size: 1.8rem;
-        padding-top: 2rem;
-        padding-bottom: 2rem;
-      }
-
-      .foooter-title {
-        text-align: left;
-        padding: 2rem;
-      }
-
-      .img-header {
-        display: block;
-        margin: 0 auto;
-      }
-
-      .footer {
-        position: relative;
-        height: 12rem;
-      }
-
-      .img-footer {
-        display: block;
-        margin-left: 4rem;
-      }
-
-      .footer span {
-        position: absolute;
-        margin-left: 7rem;
-      }
-
-      .button-link {
-        color: #58b15a;
-      }
-
-      .footer-link {
-        position: absolute;
-        margin-left: 7rem;
-        bottom: 2rem;
-
-        font-size: 1rem;
-        color: rgb(0, 0, 0);
-        text-decoration: underline;
-      }
-
-      .footer-link {
-        position: absolute;
-        margin-left: 7rem;
-
-        font-size: 1rem;
-        color: #d2dad2;
-      }
-
-      .leaf-icon {
-        width: 20px;
-        color: #58b15a;
-      }
-
-      button {
-        height: 5rem;
-        width: 18rem;
-        padding: 1rem 3rem 1rem 3rem;
-        border: none;
-        background-color: black;
-
-        font-family: inherit;
-
-        cursor: pointer;
+        color: black;
+        width: 50%;
+        font-family: Georgia, 'Times New Roman', Times, serif;
       }
 
       a {
+        font-family: inherit;
+        color: black;
         text-decoration: none;
-        font-size: 1.6rem;
+      }
+
+      p {
+        font-size: 1.4rem;
+      }
+
+      button {
+        padding: 1rem;
+        background: #58b15a;
+        border: none;
       }
     </style>
-    
-        <meta charset="UTF-8" />
-        <meta http-equiv="Content-Type" , content="text/html; charset=UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    
-        <title>${this.options.subject}</title>
-      </head>
-      <body>
-        <div class="container">
-          <header class="header">
-            <img class="img-header" src="${this.options.url}/public/emailImgs/vesearch-logo.png" height="120px" />
-          </header>
-    
-          <main class="main">
-            <div class="main-body">
-              <h1>Hey, ${this.options.username}! 👋</h1>
-              <p class="main-body-para">
-                ...Or should we say, Bye 😭. 
-                <br />
-                <br />
-                From today, you'll have 14 days to sign back into your account to
-                reactivate it. After 14 days, if you haven't signed back in, your
-                account will be deleted, however, all your  yummy recipes will
-                remain for everyone to view. If, for some reason, you also wish for
-                all your recipes to be removed, please email hello(at)VESearch.com,
-                to request this.
-              </p>
-              <img
-                class="leaf-icon"
-                src="${this.options.url}/public/emailImgs/leaf-icon.svg"
-                alt="leaf icon svg"
-              />
-              <img
-                class="leaf-icon"
-                src="${this.options.url}/public/emailImgs/leaf-icon.svg"
-                alt="leaf icon svg"
-              />
-              <img
-                class="leaf-icon"
-                src="${this.options.url}/public/emailImgs/leaf-icon.svg"
-                alt="leaf icon svg"
-              />
-              <p class="main-body-para">
-                If you ultimately maintain that deleting your account is the right
-                decision for you. Please remember you are always welcome to use
-                VESearch to search your favourite recipes. Please visit us again
-                soon.
-              </p>
-              <button class="main-body-link">
-                <a class="button-link" href="${this.options.url}">VESearch</a>
-              </button>
-            </div>
-          </main>
-    
-          <footer class="footer">
-            <img class="img-footer" src="${this.options.url}/public/emailImgs/vesearch-logo.png" height="100px" />
-            <a class="footer-link" href="https://www.jameskevinwoodruff.com"
-              >By Atomic Mammoth Media</a
-            >
-          </footer>
-        </div>
-      </body>
-    </html>`;
+
+    <meta charset="UTF-8" />
+    <meta http-equiv="Content-Type" , content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>${this.options.subject}</title>
+  </head>
+  <body>
+    <div>
+    <main>
+    <div >
+      <h1>Hey, ${this.options.username}! 👋</h1>
+      <p>
+        ...Or should we say, Bye 😭. 
+        <br />
+        <br />
+        From today, you'll have 14 days to sign back into your account to
+        reactivate it. After 14 days, if you haven't signed back in, your
+        account will be deleted, however, all your  yummy recipes will
+        remain for everyone to view. If, for some reason, you also wish for
+        all your recipes to be removed, please email hello(at)VESearch.com,
+        to request this.
+      </p>
+      <p >
+        If you ultimately maintain that deleting your account is the right
+        decision for you. Please remember you are always welcome to use
+        VESearch to search your favourite recipes. Please visit us again
+        soon.
+      </p>
+      <button>
+        <a href="${this.options.url}">VESearch</a>
+      </button>
+    </div>
+  </main>
+    </div>
+  </body>
+</html>
+    `;
   }
 };
