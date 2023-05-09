0ea604b744ded70ae1e0441f6f0a124f4b00fa3b
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-05-09 17:37:50 +0100

    big fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,7 +22,7 @@ exports.resizeUserPhoto = (req, res, next) => {
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 80 })
-    .toFile(`${__dirname}/public/imgs/${req.file.filename}`);
+    .toFile(`${__dirname}/../public/imgs/${req.file.filename}`);
 
   next();
 };
