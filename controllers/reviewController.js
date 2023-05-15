c2775767f2dbc0464891957996ece09d310dcd68
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-05-15 23:26:25 +0100

    added endpoint to delete a review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Review = require('../models/reviewModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.addReview = catchAsync(async (req, res, next) => {
@@ -33,6 +34,23 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
       reviews,
     },
   });
+});
+
+exports.deleteOneReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findById(req.params.review_id);
+
+  if (!review) {
+    return next(new AppError('No review by that ID', 404));
+  }
+
+  if (req.user.id !== review?.author.id)
+    return next(
+      new AppError('You do not have permission to perform this action', 403)
+    );
+
+  await Review.findByIdAndDelete(review.id);
+
+  res.status(204).send();
 });
 
 exports.ratingsAverage = catchAsync(async (req, res, next) => {
