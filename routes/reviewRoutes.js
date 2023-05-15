ce3e940eea3cebc349429fa733dd44775b636eb8
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-05-15 23:27:19 +0100

    added new route

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -3,12 +3,15 @@ const {
   addReview,
   getAllReviews,
   ratingsAverage,
+  deleteOneReview,
 } = require('../controllers/reviewController');
 const { protect } = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/ratingsAverage/:recipeId').get(ratingsAverage);
+
+router.route('/:review_id').delete(protect, deleteOneReview);
 
 // the rerouted route from the recipe route will come in here along with the params
 router.route('/').post(protect, addReview).get(protect, getAllReviews);
