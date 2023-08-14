37e6cb1b1669cd1b6f671a9e4f14b26326b4e170
“jahjam” <elijahjamesuk@hotmail.com>
2023-08-14 21:43:29 +0100

    add middleware to generate average rating when review is added

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,73 +2,99 @@ const mongoose = require('mongoose');
 const Review = require('../models/reviewModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const Recipe = require("../models/recipeModel");
+
+exports.calculateAverageRating = catchAsync(async (req, res, next) => {
+    const {ObjectId} = mongoose.Types;
+
+    const ratings = await Review.aggregate([
+        {$match: {recipe: ObjectId(req.params.recipeId)}},
+        {
+            $group: {
+                _id: null,
+                avgRatings: {$avg: '$rating'},
+            },
+        },
+    ]);
+
+    await Recipe.findByIdAndUpdate(
+        req.params.recipeId,
+        {
+            ratingsAverage: ratings[0].avgRatings,
+        }, {
+            new: true,
+        }
+    );
+
+    next();
+})
 
 exports.addReview = catchAsync(async (req, res, next) => {
-  const review = await Review.create({
-    rating: req.body.rating,
-    comment: req.body.comment,
-    author: req.user.id,
-    date: Date.now(),
-    recipe: req.params.recipeId,
-  });
+    const review = await Review.create({
+        rating: req.body.rating,
+        comment: req.body.comment,
+        author: req.user.id,
+        date: Date.now(),
+        recipe: req.params.recipeId,
+    });
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      review,
-    },
-  });
+    res.status(201).json({
+        status: 'success',
+        data: {
+            review,
+        },
+    });
 });
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
+    let filter = {};
 
-  if (req.params.recipeId) filter = { recipe: req.params.recipeId };
+    if (req.params.recipeId) filter = {recipe: req.params.recipeId};
 
-  const reviews = await Review.find(filter);
+    const reviews = await Review.find(filter);
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      reviews,
-    },
-  });
+    res.status(201).json({
+        status: 'success',
+        data: {
+            reviews,
+        },
+    });
 });
 
 exports.deleteOneReview = catchAsync(async (req, res, next) => {
-  const review = await Review.findById(req.params.review_id);
+    const review = await Review.findById(req.params.review_id);
 
-  if (!review) {
-    return next(new AppError('No review by that ID', 404));
-  }
+    if (!review) {
+        return next(new AppError('No review by that ID', 404));
+    }
 
-  if (req.user.id !== review?.author.id)
-    return next(
-      new AppError('You do not have permission to perform this action', 403)
-    );
+    if (req.user.id !== review?.author.id)
+        return next(
+            new AppError('You do not have permission to perform this action', 403)
+        );
 
-  await Review.findByIdAndDelete(review.id);
+    await Review.findByIdAndDelete(review.id);
 
-  res.status(204).send();
+    res.status(204).send();
 });
 
 exports.ratingsAverage = catchAsync(async (req, res, next) => {
-  const { ObjectId } = mongoose.Types;
+    const {ObjectId} = mongoose.Types;
 
-  const ratings = await Review.aggregate([
-    { $match: { recipe: ObjectId(req.params.recipeId) } },
-    {
-      $group: {
-        _id: null,
-        avgRatings: { $avg: '$rating' },
-      },
-    },
-  ]);
+    const ratings = await Review.aggregate([
+        {$match: {recipe: ObjectId(req.params.recipeId)}},
+        {
+            $group: {
+                _id: null,
+                avgRatings: {$avg: '$rating'},
+            },
+        },
+    ]);
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      ratings,
-    },
-  });
+    res.status(201).json({
+        status: 'success',
+        data: {
+            ratings,
+        },
+    });
 });
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const {
-  addReview,
-  getAllReviews,
-  ratingsAverage,
-  deleteOneReview,
+    addReview,
+    getAllReviews,
+    ratingsAverage,
+    deleteOneReview,
+    calculateAverageRating,
 } = require('../controllers/reviewController');
-const { protect } = require('../controllers/authController');
+const {protect} = require('../controllers/authController');
 
-const router = express.Router({ mergeParams: true });
+const router = express.Router({mergeParams: true});
 
 router.route('/ratingsAverage/:recipeId').get(ratingsAverage);
 
 router.route('/:review_id').delete(protect, deleteOneReview);
 
 // the rerouted route from the recipe route will come in here along with the params
-router.route('/').post(protect, addReview).get(protect, getAllReviews);
+router.route('/').post(protect, calculateAverageRating, addReview).get(protect, getAllReviews);
 
 module.exports = router;
